2fb36d8aab0024847bb97c307472aa9b064112e5
James McFarland <james@jamess-mbp.home>
2014-08-12 22:37:48 -0400

    changed info timing calcs

--- bruce/sacModFinal/runBatchAnalysis.m
+++ bruce/sacModFinal/runBatchAnalysis.m
@@ -163,6 +163,45 @@ for Elist_cnt = 1:length(Expt_list)
 end
 
 
+%% LEM sac_info_timing
+clear all
+addpath('~/James_scripts/bruce/eye_tracking_improvements/');
+addpath('~/James_scripts/bruce/sacModFinal/');
+Expt_list = {'M266','M270','M275','M277','M281','M287','M289','M294','M296'};
+global Expt_name bar_ori
+bar_ori = nan; 
+for Elist_cnt = 1:length(Expt_list)
+    Expt_name = Expt_list{Elist_cnt};
+    info_timing_calcs;
+    clearvars -except Elist_cnt Expt_list Expt_name bar_ori
+end
+
+Expt_name = 'M297'; bar_ori = 0;
+info_timing_calcs;
+Expt_name = 'M297'; bar_ori = 90;
+info_timing_calcs;
+
+%% JBE sacStimMOd Analysis 
+clear all
+addpath('~/James_scripts/bruce/eye_tracking_improvements/');
+addpath('~/James_scripts/bruce/sacModFinal/');
+Expt_list = {'G085','G086','G087','G088','G089','G091','G093','G095'};
+global Expt_name bar_ori
+bar_ori = 0;
+for Elist_cnt = 1:length(Expt_list)
+    Expt_name = Expt_list{Elist_cnt};
+    info_timing_calcs;
+    clearvars -except Elist_cnt Expt_list Expt_name bar_ori
+end
+
+Expt_list = {'G085','G086','G087','G088','G089','G091','G093'};
+bar_ori = 90;
+for Elist_cnt = 1:length(Expt_list)
+    Expt_name = Expt_list{Elist_cnt};
+    info_timing_calcs;
+    clearvars -except Elist_cnt Expt_list Expt_name bar_ori
+end
+
 
 
 %% LEM LFP Analysis
