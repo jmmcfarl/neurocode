862e3f25c842366dd40ce13133b3ca90b48ee26c
James <james@CA1.(none)>
2014-12-11 16:24:12 -0500

    minor

--- bruce/choiceProb/initial_testing.m
+++ bruce/choiceProb/initial_testing.m
@@ -23,6 +23,7 @@ for nn = 1:Nunits
     MUchans(end+1) = AllExpt.Header(nn).probe;
   end    
 end
+SUchans = round(SUchans);
 
 %%
 trialNums = [AllExpt.Expt.Trials(:).Trial];
@@ -45,6 +46,8 @@ for tr = 1:Ntrials
   end 
 end
 totSpkCnts = squeeze(nansum(fullRobs));
+avg_spk_rates = nanmean(reshape(fullRobs,[],length(SUindx)));
+fullRobs_ms = bsxfun(@minus,fullRobs,reshape(avg_spk_rates,[1 1 length(SUindx)]));
 %%
 LFP_trial_taxis = AllExpt.Expt.Header.LFPtimes*1e-4;
 R_trial_taxis = (0:(NT-1))*dt + dt/2;
@@ -53,7 +56,7 @@ nprobes = 24;
 
 trial_LFP_state = nan(NT,Ntrials,nprobes);
 
-lcf = 2; hcf = 4; %cut-off freqs for LFP filter
+lcf = 10; hcf = 20; %cut-off freqs for LFP filter
 [filt_bb,filt_aa] = butter(2,[lcf hcf]/(LFP_Fs/2));
 for tr = 1:Ntrials
     cur_LFPs = double(AllExpt.Expt.Trials(tr).LFP);
@@ -65,3 +68,48 @@ for tr = 1:Ntrials
 end
 
 %%
+% test_trials = find(trialOB == 130);
+test_trials = 1:Ntrials;
+un_stim_seeds = unique(trialSe(test_trials));
+
+targChan = 22;
+
+beg_buff = 15; %number of bins from beginning of trial to exclude
+
+maxlag_ED = pi/2;
+ED_space = pi/20;
+ED_bin_edges = 0:ED_space:maxlag_ED;
+ED_bin_centers = (ED_bin_edges(1:end-1)+ED_bin_edges(2:end))/2;
+
+cur_XC = zeros(length(ED_bin_centers),length(SUindx));
+cur_cnt = zeros(length(ED_bin_centers),length(SUindx));
+rand_XC = zeros(1,length(SUindx));
+rand_cnt = zeros(1,length(SUindx));
+for tr = 1:length(un_stim_seeds)
+    cur_tr_set = find(trialSe == un_stim_seeds(tr));
+%     cur_tr_set(trialOB(cur_tr_set) ~= 130) = [];
+    fprintf('Tr %d, %d rpts\n',tr,length(cur_tr_set));
+    if length(cur_tr_set) >= 2
+        [II,JJ] = meshgrid(1:length(cur_tr_set));
+        
+        cur_LFP_data = squeeze(trial_LFP_state(:,cur_tr_set,targChan));
+        for tt = (beg_buff+1):NT
+            cur_Robs = squeeze(fullRobs_ms(tt,cur_tr_set,:));
+%             cur_Robs = squeeze(fullRobs(tt,cur_tr_set,:));
+            cur_Dmat = abs(circ_dist2(cur_LFP_data(tt,:),cur_LFP_data(tt,:)));
+            cur_Dmat(logical(eye(length(cur_tr_set)))) = nan;
+            
+            for jj = 1:length(ED_bin_centers)
+                curset = find(cur_Dmat > ED_bin_edges(jj) & cur_Dmat <= ED_bin_edges(jj+1));
+                cur_XC(jj,:) = cur_XC(jj,:) + squeeze(nansum(bsxfun(@times,cur_Robs(II(curset),:),cur_Robs(JJ(curset),:)),1));
+                cur_cnt(jj,:) = cur_cnt(jj,:) + sum(~(isnan(cur_Robs(II(curset),:)) & ~(isnan(cur_Robs(JJ(curset),:)))));
+            end
+            curset = ~isnan(cur_Dmat);
+            rand_XC = rand_XC + squeeze(nansum(bsxfun(@times,cur_Robs(II(curset),:),cur_Robs(JJ(curset),:)),1));
+            rand_cnt = rand_cnt + nansum(~(isnan(cur_Robs(II(curset),:))) & ~(isnan(cur_Robs(JJ(curset),:))));
+        end
+    end
+end
+stateDepVar = cur_XC./cur_cnt;
+randVar = rand_XC./rand_cnt;
+
