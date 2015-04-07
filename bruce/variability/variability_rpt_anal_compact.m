52432691ece50ce823bd82fc5ced26a5e5b116aa
James <james@CA1.(none)>
2015-04-06 22:33:26 -0400

    variability rpt script

--- bruce/variability/variability_rpt_anal_compact.m
+++ bruce/variability/variability_rpt_anal_compact.m
@@ -13,6 +13,8 @@ fit_unCor = false; %use eye correction
 use_MUA = false; %use MUA in model-fitting
 fit_rect = false; %split quad linear filter into two rectified
 use_hres_ET = true; %use high-res eye-tracking?
+exclude_sacs = true;
+base_dt = 0.005;
 
 use_LOOXV = 1; %[0 is no LOO; 1 is SUs only; 2 is SU + MU]
 
@@ -99,6 +101,8 @@ SU_numbers = Clust_data.SU_numbers;
 n_units = size(all_binned_mua,2) + size(all_binned_sua,2);
 
 %% extract time series to keep track of trial number and block number
+orig_dt = params.dt;
+
 NT = length(used_inds);
 fullNT = size(all_binned_mua,1);
 n_trials = length(time_data.trial_flip_ids);
@@ -125,30 +129,6 @@ for i = 1:n_blocks
     Xblock(cur_set,i) = 1;
 end
 
-%% get binned spike data on finer time scale
-orig_dt = params.dt;
-base_dt = 0.005;
-
-if n_trials ~= length(trial_data)
-    error('Trial data mismatch');
-end
-up_nf = unique(expt_data.expt_nf)/base_dt*orig_dt;
-tbt_binned_spikes = nan(up_nf,n_trials,length(SU_numbers));
-tbt_t_axis = nan(up_nf,n_trials);
-for ii = 1:n_trials
-    cur_bin_edges = [trial_data(ii).start_times:base_dt:(trial_data(ii).start_times + base_dt*(up_nf))];
-    cur_bin_cents = 0.5*cur_bin_edges(1:end-1) + 0.5*cur_bin_edges(2:end);
-    for cc = 1:length(SU_numbers)
-       cur_hist = histc(spike_data.SU_spk_times{cc},cur_bin_edges);
-       tbt_binned_spikes(:,ii,cc) = cur_hist(1:end-1);
-    end
-    tbt_t_axis(:,ii) = cur_bin_cents;
-end
-
-orig_t_ind = round(interp1(time_data.t_axis,1:fullNT,tbt_t_axis(:)));
-orig_t_ind(1) = 1;
-up_used_inds = find(ismember(orig_t_ind,used_inds));
-
 %% CREATE SACCADE PREDICTOR MATS
 corrected_eye_vals_interp = ET_data.interp_eye_pos;
 sac_start_times = [ET_data.saccades(:).start_time];
@@ -166,6 +146,24 @@ saccade_stop_inds(isnan(saccade_stop_inds)) = length(used_inds);
 used_is_blink = ET_data.is_blink(used_saccade_set);
 
 saccade_trial_inds = all_trialvec(used_inds(saccade_start_inds));
+
+saccades = ET_data.saccades(used_saccade_set);
+
+sac_durs = [saccades(:).duration];
+sac_prepos = reshape([saccades(:).pre_pos],[],length(saccades));
+sac_postpos = reshape([saccades(:).post_pos],[],length(saccades));
+sac_deltaX = sac_postpos(1,:) - sac_prepos(1,:);
+
+sacburst_set = find([saccades(:).isi] < params.sac_burst_isi | [saccades(:).next_isi] < params.sac_burst_isi);
+micro_sacs = find([saccades(:).amplitude] < params.micro_thresh & ~used_is_blink');
+
+msac_bursts = micro_sacs(ismember(micro_sacs,sacburst_set));
+% micro_sacs(ismember(micro_sacs,sacburst_set)) = []; %eliminate microsacs that are part of a 'burst'
+
+%guided saccades are those whose parallel component is large enough and
+%that aren't blinks (and whose duration is not too long to be suspicious
+big_sacs = find(abs(sac_deltaX) > params.gsac_thresh & ~used_is_blink' & sac_durs <= params.max_gsac_dur);
+used_is_blink(sac_durs >= params.max_gsac_dur) = true;
 
 %% DEFINE FIXATIONS
 trial_start_inds = [1; find(diff(all_trialvec(used_inds)) ~= 0) + 1];
@@ -248,34 +246,86 @@ n_rpts = length(rpt_trials);
 all_rpt_inds = find(ismember(all_trialvec(used_inds),rpt_trials));
 all_nonrpt_inds = find(~ismember(all_trialvec(used_inds),rpt_trials));
 
+%% get binned spike data on finer time scale
+
+if n_trials ~= length(trial_data)
+    error('Trial data mismatch');
+end
+up_nf = unique(expt_data.expt_nf)/base_dt*orig_dt;
+tbt_binned_spikes = nan(up_nf,n_rpts,length(SU_numbers));
+tbt_t_axis = nan(up_nf,n_rpts);
+for ii = 1:n_rpts
+    cur_bin_edges = [trial_data(rpt_trials(ii)).start_times:base_dt:(trial_data(rpt_trials(ii)).start_times + base_dt*(up_nf))];
+    cur_bin_cents = 0.5*cur_bin_edges(1:end-1) + 0.5*cur_bin_edges(2:end);
+    for cc = 1:length(SU_numbers)
+       cur_hist = histc(spike_data.SU_spk_times{cc},cur_bin_edges);
+       tbt_binned_spikes(:,ii,cc) = cur_hist(1:end-1);
+    end
+    tbt_t_axis(:,ii) = cur_bin_cents;
+end
+
+orig_t_ind = round(interp1(time_data.t_axis,1:fullNT,tbt_t_axis(:)));
+orig_t_ind(1) = 1;
+up_used_inds = find(ismember(orig_t_ind,used_inds));
+
+tbt_binned_spikes = reshape(tbt_binned_spikes,[],length(SU_numbers));
+for ii = 1:length(SU_numbers)
+   tbt_binned_spikes(isnan(all_binned_sua(orig_t_ind,ii)),ii) = nan; 
+end
+tbt_binned_spikes = reshape(tbt_binned_spikes,up_nf,n_rpts,length(SU_numbers));
+
 %% DEFINE IN-SAC AND IN-BUFF INDS
-postsac_buff = round(0.03/base_dt);
+postsac_buff = round(0.03/base_dt); %window after saccade stop time to exclude data
 rpt_saccade_set = find(ismember(saccade_trial_inds,rpt_trials));
 
-in_sac_inds = zeros(up_nf*n_trials,1);
-for ii = 1:sac_buff+1
-    cur_inds = saccade_stop_inds + (ii-1);
-    cur_inds(cur_inds > NT) = [];
-    in_sac_inds(cur_inds) = 1;
-end
-for ii = 1:length(saccade_start_inds)
-    cur_inds = saccade_start_inds(ii):saccade_stop_inds(ii);
-    in_sac_inds(cur_inds) = 1;
+rpt_sac_start_inds = round(interp1(tbt_t_axis(:),1:length(tbt_t_axis(:)),sac_start_times(used_saccade_set(rpt_saccade_set))));
+rpt_sac_stop_inds = round(interp1(tbt_t_axis(:),1:length(tbt_t_axis(:)),sac_stop_times(used_saccade_set(rpt_saccade_set))));
+
+in_sac_inds = zeros(up_nf*n_rpts,1);
+in_blink_inds = zeros(up_nf*n_rpts,1);
+for ii = 1:length(rpt_saccade_set)
+    cur_ind_range = rpt_sac_start_inds(ii):(rpt_sac_stop_inds(ii) + postsac_buff);
+    cur_ind_range(all_trialvec(orig_t_ind(cur_ind_range)) ~= all_trialvec(orig_t_ind(cur_ind_range(1)))) = [];
+    
+    if ~used_is_blink(rpt_saccade_set(ii)) 
+    in_sac_inds(cur_ind_range) = 1;
+    else
+    in_blink_inds(cur_ind_range) = 1;
+    end
 end
 
-in_sac_inds = logical(in_sac_inds);
+in_sac_inds = logical(reshape(in_sac_inds,up_nf,n_rpts));
+in_blink_inds = logical(reshape(in_blink_inds,up_nf,n_rpts));
 
-blink_inds = find(used_is_blink);
-in_blink_inds = zeros(NT,1);
-for ii = 1:length(blink_inds)
-    cur_inds = saccade_start_inds(blink_inds(ii)):saccade_stop_inds(blink_inds(ii));
-    in_blink_inds(cur_inds) = 1;
+%% process trial-by-trial binned spike data (exclude blinks, sacs, subtract trial avgs)
+tbt_BS_ms = reshape(tbt_binned_spikes,[],length(SU_numbers));
+tbt_BS_ms(in_blink_inds(:),:) = nan;
+
+if exclude_sacs
+    tbt_BS_ms(in_sac_inds(:),:) = nan;
 end
-in_blink_inds = logical(in_blink_inds);
+
+tbt_BS_ms = reshape(tbt_BS_ms,up_nf,n_rpts,length(SU_numbers));
+%subtract out trial-avg spike count
+trial_avg_BS = nanmean(tbt_binned_spikes);
+tbt_BS_ms = bsxfun(@minus,tbt_binned_spikes,nanmean(trial_avg_BS));
+trial_avg_BS = squeeze(trial_avg_BS);
+%%
+psths = squeeze(nanmean(tbt_BS_ms,2));
+psth_var = nanvar(psths);
+tot_resp_var = nanvar(reshape(tbt_BS_ms,[],length(SU_numbers)));
+
+n_utrials = squeeze(mean(sum(~isnan(tbt_BS_ms),2)));
+
+avg_temp_var = squeeze(nanmean(nanvar(tbt_BS_ms))); %avg (across trials) of across-time variance
+psth_var_cor = psth_var.*(n_utrials'./(n_utrials'-1)) - avg_temp_var'./n_utrials'; %sahani linden correction for PSTH sampling noise
+
+trial_avg_var = squeeze(nanvar(trial_avg_BS)); %variance of trial-avg rates
+
 
 %%
 interp_post_mean_EP = interp1(time_data.t_axis(used_inds),post_mean_EP,tbt_t_axis(up_used_inds));
 tbt_EP = nan(up_nf,n_trials);
 tbt_EP(up_used_inds) = interp_post_mean_EP;
 
-
+%%
